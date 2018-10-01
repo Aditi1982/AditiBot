c02377e78ef40ba2afafe4a14e351881205ade9b
Aditi1982 <aditisingh1982@gmail.com>
2018-09-30 21:48:42 -0700

    Delete publish.cmd

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=aditestbot-Web-Deploy.pubxml -p:Password=pfAmmDfATQuvcv4BCPhoengfKN9lfXqpiCxxxPCbTydDCzq84JhdDdKT2NCg
-
